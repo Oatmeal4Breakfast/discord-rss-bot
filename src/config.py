bfb072e53c448616385c5d01e4ae85d3c765e54f
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-02-08 15:00:33 -0500

    refactor: split the config and feeds

--- src/config.py
+++ src/config.py
@@ -1,50 +1,11 @@
-import yaml
-from pathlib import Path
-from typing import Any, Tuple
-from src.models import Config, FeedEntries
-
-script_dir: Path = Path.cwd()
-config_path: Path = script_dir / "config.yaml"
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-def load_config(config_file: Path | str) -> dict[str, Any]:
-    """
-    returns yaml object from the config file
-    """
-
-    try:
-        with open(config_file, mode="r") as file:
-            return yaml.safe_load(stream=file)
-    except FileNotFoundError:
-        raise
-    except FileExistsError:
-        raise
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+    retry_count: int = Field(alias="RETRY_COUNT")
+    sent_file: str = Field(alias="SENT_FILE")
 
 
-def parse_config(loaded_config: dict[str, Any]) -> Tuple[Config, FeedEntries]:
-    """
-    Parse the config object return by the load function and return
-    feeds and return config object
-    """
-    config: Config = Config(
-        default_webhook=loaded_config.get("default_webhook", ""),
-        rate_limit_delay=loaded_config.get("rate_limit_delay", 0.5),
-        batch_size=loaded_config.get("batch_size", 5),
-        filter_by_today=loaded_config.get("filter_by_today", True),
-    )
-
-    feed_items: FeedEntries = loaded_config.get("feeds", [])
-
-    return config, feed_items
-
-
-if __name__ == "__main__":
-    print(f"config path: {config_path}")
-    print("loading config file now....")
-    loaded_config = load_config(config_path)
-    print(f"raw config object: {loaded_config}")
-    print("=" * 50)
-    config, feed_items = parse_config(loaded_config)
-    print(f"Config: {config}")
-    print("=" * 50)
-    print(f"feed_items: {feed_items}")
+config = Config()
--- src/feed_handler.py
+++ src/feed_handler.py
@@ -1,31 +1,53 @@
 import feedparser
+import yaml
+from pathlib import Path
 from feedparser import FeedParserDict
+
 from src.models import Entry
 
 
-def fetch_feed_entries(url: str, max_items: int = 5) -> list[Entry]:
-    """
-    Fetch entries from the feed up to the max items set in the config.
-    Defaults to 5
+class FeedHandler:
+    def __init__(self) -> None:
+        self.feeds_file: Path = Path.cwd() / "feeds.yaml"
 
-    Returns a list of entries
-    """
-    feed: FeedParserDict = feedparser.parse(url_file_stream_or_string=url)
-    entries: list[Entry] = []
-    for entry in feed.entries[:max_items]:
-        new_entry: Entry = Entry(
-            title=entry.title,
-            link=entry.link,
-            published=entry.published,
-            summary=entry.summary if "summary" in entry else "",
-        )
-        entries.append(new_entry)
-    if feed.bozo:
-        return []
-    return entries
+    def load_feed(self) -> dict[str, str]:
+        """
+        returns yaml object from the feeds file
+        """
+        try:
+            with open(file=self.feeds_file, mode="r") as file:
+                return yaml.safe_load(stream=file)
+        except FileNotFoundError:
+            raise
+        except FileExistsError:
+            raise
+
+    def fetch_feed_entries(self, url: str, max_items: int = 5) -> list[Entry]:
+        """
+        Fetch entries from the feed up to the max items set in the config.
+        Defaults to 5
+
+        Returns a list of entries
+        """
+        feed: FeedParserDict = feedparser.parse(url_file_stream_or_string=url)
+        entries: list[Entry] = []
+        for entry in feed.entries[:max_items]:
+            new_entry: Entry = Entry(
+                title=entry.title,
+                link=entry.link,
+                published=entry.published,
+                summary=entry.summary if "summary" in entry else "",
+            )
+            entries.append(new_entry)
+        if feed.bozo:
+            return []
+        return entries
 
 
 if __name__ == "__main__":
-    entries = fetch_feed_entries(url="https://www.reddit.com/r/FastAPI/.rss")
+    feeds_handler = FeedHandler()
+    entries: list[Entry] = feeds_handler.fetch_feed_entries(
+        url="https://www.reddit.com/r/FastAPI/.rss"
+    )
     print(entries)
     print(len(entries))
