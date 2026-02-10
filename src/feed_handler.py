0915001ecbbbe8fad8159448a0d6fdf5aac9385a
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-02-09 19:35:20 -0500

    refactor: feed_handler has more than one dataclass modeling what we will be extracting

--- src/feed_handler.py
+++ src/feed_handler.py
@@ -2,34 +2,56 @@ import feedparser
 import yaml
 from pathlib import Path
 from feedparser import FeedParserDict
+from dataclasses import dataclass
 
-from src.models import Entry
+feeds_pathh: Path | str = Path.cwd() 
+
+# TODO: Add default feeds yaml file to method for fetching entries
+# TODO: use the Feed Dataclass to clean up the data that I'll be extracting from each list items
+# TODO: Implement storage feature. Use a JSON that stores the title, the feed it was sent from, and when it was sent
+# TODO: Implement feature to read and save to the aforementioned file
 
 
+@dataclass
+class Feed:
+    name: str
+    url: str
+    webhook: str
+
+
+@dataclass
+class Entry:
+    title: str
+    link: str
+    published: str
+    summary: str
+
+
+@dataclass
 class FeedHandler:
-    def __init__(self) -> None:
-        self.feeds_file: Path = Path.cwd() / "feeds.yaml"
+    feeds: list[str] = self.load_feeds() # Fix this bug. Need to learn out to implement the call. 
+    sent_entries: list[str]
+    def load_feeds(self, feeds_file: Path | str = ) -> dict[str, str] | None:
 
-    def load_feed(self) -> dict[str, str]:
         """
         returns yaml object from the feeds file
         """
         try:
-            with open(file=self.feeds_file, mode="r") as file:
-                return yaml.safe_load(stream=file)
+            with open(file=feeds_file, mode="r") as file:
+                file_data: dict[str, str] = yaml.safe_load(stream=file)
         except FileNotFoundError:
             raise
         except FileExistsError:
             raise
 
-    def fetch_feed_entries(self, url: str, max_items: int = 5) -> list[Entry]:
+    def fetch_feed_entries(self, feed_url: str, max_items: int = 5) -> list[Entry]:
         """
         Fetch entries from the feed up to the max items set in the config.
         Defaults to 5
 
         Returns a list of entries
         """
-        feed: FeedParserDict = feedparser.parse(url_file_stream_or_string=url)
+        feed: FeedParserDict = feedparser.parse(url_file_stream_or_string=feed_url)
         entries: list[Entry] = []
         for entry in feed.entries[:max_items]:
             new_entry: Entry = Entry(
@@ -45,8 +67,7 @@ class FeedHandler:
 
 
 if __name__ == "__main__":
-    feeds_handler = FeedHandler()
-    entries: list[Entry] = feeds_handler.fetch_feed_entries(
+    entries: list[Entry] = fetch_feed_entries(
         url="https://www.reddit.com/r/FastAPI/.rss"
     )
     print(entries)
