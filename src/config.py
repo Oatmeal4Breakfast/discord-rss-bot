81eebf37873d2772e29d7133bad4f684e01fbe3d
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-02-10 21:18:31 -0500

    add: sent_file and feed_file as config fields

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     retry_count: int = Field(alias="RETRY_COUNT")
     sent_file: str = Field(alias="SENT_FILE")
+    feed_file: str = Field(alias="FEED_FILE")
 
 
 config = Config()
