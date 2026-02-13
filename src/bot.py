2478a08355547e1f5ba5b0932c960bfa61bddaa5
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-02-12 21:21:11 -0500

    fix: _add_sent_entry data types, add error handling to _save_sent_entry and a new prune method

--- src/bot.py
+++ src/bot.py
@@ -1,46 +1,105 @@
 import json
+import httpx
 from html2text import HTML2Text
 from pathlib import Path
+from dataclasses import asdict
 
-from src.config import Config, config
+from src.config import Config
 from src.feed_handler import FeedData, FeedHandler, Feed, Entry
+
+
+# TODO: Work out the logic for processing
+
+# TODO: Complete the send patch implementation
 
 
 class DiscordBot:
     def __init__(self, config: Config, parser: HTML2Text) -> None:
         self.config: Config = config
         self.parser: HTML2Text = parser
-        self.sent_items: list[dict[str, str]] = self._load_sent_items()
-        
-    def _load_sent_items(self) -> None:
-        """
-        loads the json for the sent entries
-        """
-        sent_items: Path = Path.cwd() / "sent_items.json"
-        with open(file=sent_items, mode="r") as file:
-            sent_items = json.load(file)
-            self.sent_items.append(sent_items)
+        self.sent_items: dict[str, list[str]] = self._load_sent_items()
+        self.path: Path = Path.cwd() / f"{config.sent_file}"
 
+    def _load_sent_items(self) -> dict[str, list[str]]:
+        """Creates a sent_items.json to store sent entries
 
-    def _parse_html(self, description_html: str) -> str:
-        """
-        Parse the html in the description of the entry to send plain ASCII strings to Discord
-        """
-        return self.parser.handle(data=description_html)
+        Args:
+            None
 
-    def _validate_entries(self, entries: FeedEntries) -> FeedEntries:
-        valid_entries: FeedEntries = []
+        Returns:
+            dict[str,list[str]]
+        """
+        if not self.path.is_file():
+            return {}
+
+        with open(file=self.path, mode="r") as file:
+            return json.load(file)
+
+    def _add_sent_entry(self, entry: Entry) -> None:
+        """Adds an entry to the sent_items parameter
+
+        Args:
+            entry of type Entry
+
+        Returns:
+            None
+        """
+        return self.sent_items[entry.feed].append(entry.id)
+
+    def _save_sent_entry(self) -> None:
+        """saves the sent entries to the file json in the path attribute"""
+        try:
+            with open(file=self.path, mode="w") as file:
+                json.dump(self.sent_items, fp=file, indent=4)
+        except IOError:
+            print("Log: Could not write to file")
+        except PermissionError:
+            print("Invalid permissions for file system")
+
+    def _prune_sent_entries(self) -> None:
+        """Prune the first 10 entries from the list of sent entries"""
+        MAX_BEFORE_PRUNE: int = 100
+        PRUNE_AMOUNT: int = 10
+        for feed, sent_items in self.sent_items.items():
+            if len(sent_items) >= MAX_BEFORE_PRUNE:
+                del sent_items[:PRUNE_AMOUNT]
+
+    def _parse_entry_summary(self, entry: Entry) -> str:
+        """Parse the html in the description of the entry to send plain ASCII strings to Discord
+
+        Args:
+            entry: Entry object
+
+        Returns:
+            str
+        """
+        return self.parser.handle(data=entry.summary)
+
+    def _filter_entries(self, entries: list[Entry]):
+        """check the entry id against the list of sent entries
+
+        Args:
+            entries - list containing entry objects
+
+        Returns:
+            a filtered list of entries that haven't been sent yet
+        """
+        unsent_entries: list[Entry] = []
         for entry in entries:
-            if entry not in self.sent_items:
-                valid_entries.append(entry)
-        return valid_entries
+            if entry.id not in self.sent_items[entry.feed]:
+                unsent_entries.append(entry)
+        return unsent_entries
 
-    def send_batch(self, entries: FeedEntries, feed_name: str) -> None:
-        """
-        send the batch of entries from the feed
-        """
+    async def send_batch(self, entries: list[Entry]) -> None:
+        """send the batch of entries from the feed
 
-        valid_entries: FeedEntries = self._validate_entries(entries)
+        Args:
+            entries - list of entry objects
+
+        Returns:
+            None
+        """
+        valid_entries: list[Entry] = self._filter_entries(entries)
         for entry in valid_entries:
-
-        # TODO: Workout logic needed for processing. Maybe split up th processing to another method
+            async with httpx.AsyncClient() as client:
+                raise NotImplementedError
